c0b129b318b05c1c0767b85f36995f2294016596
egarpor <edugarpor@gmail.com>
2017-01-20 18:18:02 +0100

    Update discretization-fdr.R

--- discretization-fdr.R
+++ discretization-fdr.R
@@ -33,65 +33,62 @@ ci <- function(p, M) {
 }
 
 # Size as a function of K
-# M <- 1e3
-# B <- seq(1e1, 1e4, by = 1e1)
-# K <- 1:100
-# mK <- max(K)
-# av.fdr.pval <- function(b, seed = 12453252) {
-# 
-#   # A sample of p-vals with precision 1/b
-#   set.seed(seed)  
-#   samp <- matrix((sample(x = b + 1, size = mK * M, replace = TRUE) - 1)/b,
-#                  nrow = M, ncol = mK)
-#   
-#   # Extract p-values
-#   pvals <- sapply(K, function(k) {
-#     apply(samp[, 1:k, drop = FALSE], 1, function(x) min(sort(x) / (1:k)) * k)
-#   })
-# 
-#   # Power
-#   apply(pvals, 2, function(x) c(mean(x < 0.10), mean(x < 0.05), mean(x < 0.01)))
-#   
-# }
-# A <- vector("list", 6)
-# i <- 1
-# for (b in c(5e2, 1e3, 5e3, 1e4, 5e4, 1e5)) {
-#   
-#   for (j in 1:10) {
-#     
-#     A[[i]][[j]] <- t(av.fdr.pval(b = b, seed = 202345 + j * 123))
-#     
-#   }
-#   i <- i + 1
-#   cat(i, "\n")
-#   
-# }
-# 
-# Matplot: 10 trajectories of the empirical size of the FDR (indexed by the number 
+M <- 1e3
+K <- 1:50
+mK <- max(K)
+positiveCorrection <- FALSE
+av.fdr.pval <- function(b, seed = 12453252) {
+
+  # A sample of p-vals with precision 1/b
+  set.seed(seed)
+  samp <- matrix((sample(x = b + 1, size = mK * M, replace = TRUE) - 1) + 
+                   positiveCorrection, nrow = M, ncol = mK) / (b + positiveCorrection)
+
+  # Extract p-values
+  pvals <- sapply(K, function(k) {
+    apply(samp[, 1:k, drop = FALSE], 1, function(x) min(sort(x) / (1:k)) * k)
+  })
+
+  # Power
+  apply(pvals, 2, function(x) c(mean(x < 0.10), mean(x < 0.05), mean(x < 0.01)))
+
+}
+A <- vector("list", 5)
+i <- 1
+for (b in c(5e2, 1e3, 5e3, 1e4, 5e4)) {
+
+  for (j in 1:5) {
+
+    A[[i]][[j]] <- t(av.fdr.pval(b = b, seed = 202345 + j * 123))
+
+  }
+  cat(i, "\n")
+  i <- i + 1
+
+}
+
+# Matplot: 5 trajectories of the empirical size of the FDR (indexed by the number 
 # of p-values) with respect to different p-value discretizations
 library(viridis)
-cols <- viridis(6)
-#cols <- rainbow(6)
-load("discretization-fdr.RData")
-matplot(A[[1]][[1]], type = "l", ylim = c(0, 0.2), lty = 1, pch = 1, cex = 0.5,
-        col = cols[1], xlab = "Number of p-values (projections)", ylab = "FDR p-value",
-        main = "Empirical sizes for levels with M = 1000 and p-value precision 1/B",
-        cex.main = 0.95)
-for (j in 2:10) matlines(A[[1]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
+cols <- rev(viridis(6))
+#load("discretization-fdr.RData")
+matplot(A[[1]][[1]], type = "l", ylim = c(0, 0.2), lty = 1, 
+        pch = 1, cex = 0.5, col = cols[1], xlab = "Number of p-values", 
+        ylab = "Empirical size for FDR p-value", cex.main = 0.95)
+for (j in 2:5) matlines(A[[1]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5,
                         col = cols[1])
-for (j in 1:10) matlines(A[[2]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
+for (j in 1:5) matlines(A[[2]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
                         col = cols[2])
-for (j in 1:10) matlines(A[[3]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
+for (j in 1:5) matlines(A[[3]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
                          col = cols[3])
-for (j in 1:10) matlines(A[[4]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
+for (j in 1:5) matlines(A[[4]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
                          col = cols[4])
-for (j in 1:10) matlines(A[[5]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
+for (j in 1:5) matlines(A[[5]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
                          col = cols[5])
-for (j in 1:10) matlines(A[[6]][[j]], type = "l", lty = 1, pch = 1:3, cex = 0.5, 
-                         col = cols[6])
-abline(h = ci(p = 0.10, M = M), lty = 2, lwd = 2, col = 1)
-abline(h = ci(p = 0.05, M = M), lty = 2, lwd = 2, col = 1)
-abline(h = ci(p = 0.01, M = M), lty = 2, lwd = 2, col = 1)
-legend("topleft", legend = c("B = 5e2", "B = 1e3", "B = 5e3", "B = 1e4", "B = 5e4", 
-                         "B = 1e5"), col = cols, lwd = 2)
+abline(h = ci(p = 0.10, M = M), lty = 2, lwd = 1, col = 1)
+abline(h = ci(p = 0.05, M = M), lty = 2, lwd = 1, col = 1)
+abline(h = ci(p = 0.01, M = M), lty = 2, lwd = 1, col = 1)
+abline(h = c(0.10, 0.05, 0.01), lty = 3, lwd = 1, col = 1)
+legend("topleft", legend = c("B = 5e2", "B = 1e3", "B = 5e3", "B = 1e4", 
+                             "B = 5e4"), col = cols[-6], lwd = 2)
 
